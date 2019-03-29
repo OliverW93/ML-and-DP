e733d66dc244fabc692fa7b91f16d1163133fadb
ChuangWang <296991484@qq.com>
2019-03-29 17:18:45 +0800

    Initial commit

---
+++ 01_l_reg_by_tf/l_reg_mini_batch_gradient_descent.py
@@ -0,0 +1,52 @@
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+
+
+scaler = StandardScaler()
+scaled_housing_data = scaler.fit_transform(housing.data)
+scaled_housing_data_plus_bias = np.c_[np.ones((m, 1)), scaled_housing_data]
+
+n_epochs = 10
+learning_rate = 0.01
+
+X = tf.placeholder(tf.float32, shape=(None, n + 1), name="X")
+y = tf.placeholder(tf.float32, shape=(None, 1), name="y")
+theta = tf.Variable(tf.random_uniform([n + 1, 1], -1.0, 1.0, seed=42), name="theta")
+y_pred = tf.matmul(X, theta, name="predictions")
+error = y_pred - y
+mse = tf.reduce_mean(tf.square(error), name="mse")
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(mse)
+
+init = tf.global_variables_initializer()
+
+batch_size = 100
+n_batches = int(np.ceil(m / batch_size))
+
+def fetch_batch(epoch, batch_index, batch_size):
+    np.random.seed(epoch * n_batches + batch_index)  # not shown in the book
+    indices = np.random.randint(m, size=batch_size)  # not shown
+    X_batch = scaled_housing_data_plus_bias[indices] # not shown
+    y_batch = housing.target.reshape(-1, 1)[indices] # not shown
+    return X_batch, y_batch
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(n_epochs):
+        for batch_index in range(n_batches):
+            X_batch, y_batch = fetch_batch(epoch, batch_index, batch_size)
+            sess.run(training_op, feed_dict={X: X_batch, y: y_batch})
+            # mse_val = sess.run(mse, feed_dict={X: X_batch, y: y_batch})
+            # print("Epoch", epoch, "MSE =", mse_val)
+
+    best_theta = theta.eval()
+
+print("Best theta:")
+print(best_theta)
